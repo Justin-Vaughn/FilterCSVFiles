7a5369d6ee969bf2aff8f893ed215377edc3ee98
Justin Vaughn <justinvsoccer39@gmail.com>
2023-11-28 15:10:20 -0500

    init commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // read the CSV file
+
+        // take the filtering instructions
+
+        // filter the CSV file
+
+        // write the new CSV
+
+    }
+
+    public static
+}
